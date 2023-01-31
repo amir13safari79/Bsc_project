801e79f09f5541ca00fd49150d5e39cdb969d84a
Amirhossein Safari <amirhossein.safari@digikala.com>
2023-01-31 11:25:05 +0330

    update blocks

---
+++ src/data_base.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+class DataBase:
+    def __init__(self, universe_discourse_range: tuple, number_of_partitions: int):
+        self.universe_discourse_start = universe_discourse_range[0]
+        self.universe_discourse_end = universe_discourse_range[1]
+        self.number_of_partitions = number_of_partitions
+        self.partition_size = 2 * (self.universe_discourse_end - self.universe_discourse_start) / (
+                self.number_of_partitions + 1)
+        self.half_partition_size = self.partition_size / 2
+        # self.database_type = database_type
+
+    def get_rule_and_membership(self, x: int):
+        rule_membership = {}
+        if x < self.universe_discourse_start or x > self.universe_discourse_end:
+            raise ValueError(f"x = {x}, x should be between discourse_start = {self.universe_discourse_start} and discourse_end = {self.universe_discourse_end}")
+
+        if (x - self.universe_discourse_start) <= self.half_partition_size:
+            rule1 = 1
+            membership1 = (x - self.universe_discourse_start) / self.half_partition_size
+
+            rule_membership[rule1] = membership1
+
+        elif (self.universe_discourse_end - x) <= self.half_partition_size:
+            rule1 = self.number_of_partitions
+            membership1 = (self.universe_discourse_end - x) / self.half_partition_size
+
+            rule_membership[rule1] = membership1
+        else:
+            rule1 = int(np.ceil((x - self.universe_discourse_start) / self.half_partition_size))
+            rule2 = rule1 - 1
+
+            membership1 = abs(self.universe_discourse_start + (
+                        rule1 - 1) * self.half_partition_size - x) / self.half_partition_size
+            membership2 = (self.universe_discourse_start + (
+                    rule2 + 1) * self.half_partition_size - x) / self.half_partition_size
+
+            rule_membership[rule1] = membership1
+            rule_membership[rule2] = membership2
+
+        return rule_membership
+
---
+++ src/rule_base.py
@@ -0,0 +1,27 @@
+import numpy as np
+from .performance_table import get_performance_table
+
+
+class RuleBase:
+    def __init__(self, number_of_partitions: int, train=True, trained_rule_base=None):
+        # self.rules = np.random.randint(low=-10, high=10, size=(number_of_partitions + 1, number_of_partitions)).astype("float")
+        self.performance_table = get_performance_table(number_of_partitions)
+
+        if train:
+            self.rules = get_performance_table(number_of_partitions)
+        else:
+            self.rules = trained_rule_base
+
+    def update_rules(self, er_cr_rules_number: tuple, membership: float):
+        er_cr_rules_number = (er_cr_rules_number[0] - 1, er_cr_rules_number[1] - 1)
+        self.rules[er_cr_rules_number] = self.rules[er_cr_rules_number] + membership * self.performance_table[er_cr_rules_number]
+        # self.rules[er_cr_rule_number] = self.rules[er_cr_rule_number] + self.performance_table[
+        #     er_cr_rule_number]
+
+    def get_rules(self):
+        return self.rules
+
+    def get_change_in_input(self, er_cr_rules_number: tuple, membership: float):
+        er_cr_rules_number = (er_cr_rules_number[0] - 1, er_cr_rules_number[1] - 1)
+        # er_cr_rule = (error_rule_number - 1, change_in_error_rule_number - 1)
+        return membership * self.rules[er_cr_rules_number]
